ddeae22586fc96157f3174547b11bacd8be968f3
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2023-03-08 19:15:29 +0530

    create a basic project with navbar and produc components

---
+++ src/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+function Header() {
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        ></img>
+      </Link>
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Amit</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>{" "}
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>{" "}
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime </span>
+          </div>{" "}
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/StateProvider.js
@@ -0,0 +1,9 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+export const StateContext = createContext();
+
+export const StateProvier = ({ reducer, intialState, childern }) => (
+  <StateContext.Provider value={useReducer(reducer, intialState)}>
+    {childern}
+  </StateContext.Provider>
+);
